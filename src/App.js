0bd32b2657384c5f4fab09d4c4ec0d8bb0f8b2e1
unknown <isa08029612@gmail.com>
2022-10-09 20:57:53 -0500

    perdio el chivas jajaj

--- src/App.js
+++ src/App.js
@@ -1,24 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, {Fragment, useState} from 'react'
+
+
 
 function App() {
+
+
+
+ const [file,setFile]=useState(null)
+  const selectedHandler=e=>{
+    setFile( e.target.files[0])
+
+ 
+    console.log(e.target.files[0])
+  }
+  const sendHandler=()=>{
+    if(!file){
+      alert('you must upload file')
+      return
+    }
+  
+    const formdata = new FormData()
+
+    formdata.append('image', file)
+ console.log(formdata)
+ const reader = new FileReader();
+ reader.readAsDataURL(file);
+ reader.onloadend = () => {
+   const base64data = reader.result;
+   console.log(base64data);
+
+
+  let headers = new Headers();
+
+  headers.append('Content-Type', 'application/json');
+  headers.append('Accept', 'application/json');
+  headers.append('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization')
+
+  headers.append('Access-Control-Allow-Origin', '*');
+  headers.append('Access-Control-Allow-Credentials', 'true');
+
+  headers.append('GET', 'POST', 'OPTIONS');
+
+
+   
+    fetch('https://a39rxvqrma.execute-api.us-east-1.amazonaws.com/dev/add_image', {
+      method: 'POST',
+      body: base64data
+    })
+    .then(function(response) {
+      return response.json()
+    })
+
+    document.getElementById('fileinput').value = null
+
+    setFile(null)
+
+
+
+
+ 
+
+}
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Fragment>
+      <nav className='navbar navbar-dark bg-dark'>
+        <div className="container">
+           <a href='#!' className='navbar-brand'>Upload image subaccount</a>
+        </div>
+      </nav>
+      
+
+      <div className='container mt-5'>
+
+        <br></br>
+        <div className='card p-3'>
+        <div className='row'>
+          <div className='col-5'>
+          <input id="fileinput" onChange={selectedHandler} type="file"></input>
+       
+           <br></br>
+           </div>
+           <div className='col-5'>
+          <input type="text" placeholder="Add acc_id to subaccount"></input>
+  
+          </div>
+
+          <div className='col-2' >
+         
+   
+          <button onClick={sendHandler} className="form-control btn btn-primary" type="button" >Upload</button>
+         
+         </div>
+        
+        </div>
+        
+        </div>
+        
+      </div>
+      <br></br>
+
+      <nav className='navbar navbar-dark bg-dark'>
+        <div className="container">
+           <a href='#!' className='navbar-brand'>Upload image agent</a>
+        </div>
+      </nav>
+      
+
+      <div className='container'>
+        <br></br>
+        <div className='card'>
+          <input type="file"></input>
+          <br></br>
+          <input type="text" placeholder="Add sagnte_id"></input>
+          <br></br>
+          <button type="button" className="btn btn-primary">Upload</button>
+        </div>
+      </div>
+
+
+    </Fragment>
   );
 }
 
